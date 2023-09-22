45fae0fdbc84464c65999333f97a0d912fd354e8
Mario Nakazato <marionakazat@gmail.com>
2023-09-22 01:19:08 -0300

    Refactoring the password recovery screen

--- src/screens/RecoverPassword.js
+++ src/screens/RecoverPassword.js
@@ -1,20 +1,27 @@
+import { useState } from 'react'
 import { View, Text } from 'react-native'
 import ScreensSS from '../styles/ScreensSS'
-import BoxInput from '../components/BoxInput'
-import Button from '../components/Button'
-import Erro from '../components/Erro'
+import InputText from '../components/InputText'
+import Txt from '../components/Txt'
+import Emoticon from '../components/Button'
 
 const RecoverPassword = (props) => {
+    const [email, setEmail] = useState()
+
     const Cadastrar = () => {
         props.navigation.pop()
     }
     return (
         <View style={ScreensSS.conteiner}>
             <View>
-                <BoxInput value='email@provedor.com' Text='E-mail' />
-                <Erro Erro='E-mail parece ser inválido' />
+                <Txt value='E-mail' color='white' fontSize={16} />
+                <InputText value={email} placeholder='email@provedor.com' color='#3F92C5' width={512} height={32}
+                    fontSize={12}
+                    onChangeText={setEmail} keyboardType='email-address' />
+                <Txt value='E-mail parece ser inválido' color='#FD7979' fontSize={14} paddingBottom={8} />
             </View>
-            <Button Text='RECUPERAR' Execute={Cadastrar} BackgroundColor='#37BD6D' FontSize={16} Width={512} Height={32} />
+            <Emoticon value='RECUPERAR' color='white' iconColor='white' backgroundColor='#37BD6D' width={512} height={32}
+                size={0} fontSize={16} Execute={Cadastrar} />
         </View>
     )
 }
