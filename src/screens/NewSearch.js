46a72eb69ac47e15c02f74815ce77b95cbce18a8
Mario Nakazato <marionakazat@gmail.com>
2023-09-22 01:30:47 -0300

    Refactoring of the new search screen

--- src/screens/NewSearch.js
+++ src/screens/NewSearch.js
@@ -1,30 +1,50 @@
+import { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
-import Button from '../components/Button'
-import BoxInput from '../components/BoxInput'
 import ScreensSS from '../styles/ScreensSS'
-import Erro from '../components/Erro'
+import Txt from '../components/Txt'
+import InputText from '../components/InputText'
+import Emoticon from '../components/Button'
 
 const estilos = StyleSheet.create({
     conteinerCadastrar: {
         flexDirection: 'column',
         justifyContent: 'space-evenly',
-        alignItems: 'center',
     },
 })
 
 const NewSearch = (props) => {
+    const [nome, setNome] = useState()
+    const [data, setData] = useState()
+
+    const Imagem = () => {
+        console.log('Botão - Câmera/Galeria de imagens')
+    }
     const Cadastrar = () => {
         props.navigation.push('Drawer')
     }
+
     return (
         <View style={ScreensSS.conteiner}>
             <View style={estilos.conteinerCadastrar}>
-                <BoxInput value='' Text='Nome' />
-                <Erro Erro='Preencha no nome da pesquisa' />
-                <BoxInput value='' Text='Data' />
-                <Erro Erro='Preencha a data' />
-                <Button Text='CADASTRAR' Execute={Cadastrar} BackgroundColor='#37BD6D' FontSize={16} Width={512} Height={32} />
+                <View>
+                    <Txt value='Nome' color='white' fontSize={16} paddingTop={4} />
+                    <InputText value={nome} color='#3F92C5' width={512} height={32} fontSize={12}
+                        onChangeText={setNome} keyboardType='default' />
+                    <Txt value='Preencha no nome da pesquisa' color='#FD7979' fontSize={12} />
+                </View>
+                <View>
+                    <Txt value='Data' color='white' fontSize={16} paddingTop={4} />
+                    <InputText value={data} color='#3F92C5' width={512} height={32} fontSize={12}
+                        onChangeText={setData} keyboardType='default' />
+                    <Txt value='Preencha a data' color='#FD7979' fontSize={12} />
+                </View>
+                <View>
+                    <Txt value='Imagem' color='white' fontSize={16} paddingTop={4} />
+                    <Emoticon value='Câmera/Galeria de imagens' color='#939393' iconColor='white' backgroundColor='white' width={256} height={64} size={0} fontSize={16} Execute={Imagem} />
+                </View>
             </View>
+            <Emoticon value='CADASTRAR' color='white' iconColor='white' backgroundColor='#37BD6D' width={512} height={32} size={0} fontSize={16}
+                Execute={Cadastrar} />
         </View>
     )
 }
