f03299daa7dbfd6aafb22d9018d13edece5435e4
Mario Nakazato <marionakazat@gmail.com>
2023-09-22 01:33:17 -0300

    Refactoring of the search action screen

--- src/screens/SearchActions.js
+++ src/screens/SearchActions.js
@@ -1,9 +1,36 @@
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import ScreensSS from '../styles/ScreensSS'
+import Emoticon from '../components/Button'
+
+const estilos = StyleSheet.create({
+    conteiner: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        width: '100%'
+    },
+})
 
 const SearchActions = (props) => {
+    const Modificar = () => {
+        props.navigation.push('ModifySearch')
+    }
+    const Coletar = () => {
+        props.navigation.push('Collect')
+    }
+    const Relatorio = () => {
+        props.navigation.push('Report')
+    }
+
     return (
         <View style={ScreensSS.conteiner}>
+            <View style={estilos.conteiner}>
+                <Emoticon name='file-document-edit-outline' color='white' iconColor='white' backgroundColor='#312464' borderRadius={8} value='Modificar'
+                    size={80} fontSize={32} width={210} height={170} Execute={Modificar} />
+                <Emoticon name='pencil-box-multiple-outline' color='white' iconColor='white' backgroundColor='#312464' borderRadius={8}
+                    value='Coletar dados' size={80} fontSize={32} width={210} height={170} Execute={Coletar} />
+                <Emoticon name='progress-pencil' color='white' iconColor='white' backgroundColor='#312464' borderRadius={8} value='Relatório' size={80}
+                    fontSize={32} width={210} height={170} Execute={Relatorio} />
+            </View>
         </View>
     )
 }
