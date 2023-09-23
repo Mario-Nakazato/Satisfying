af830069deeaf690d12ef02dc9e4977133eacf08
Mario Nakazato <marionakazat@gmail.com>
2023-09-23 11:40:48 -0300

    Password recovery screen validation

--- src/screens/RecoverPassword.js
+++ src/screens/RecoverPassword.js
@@ -7,10 +7,19 @@ import Button from '../components/Button'
 
 const RecoverPassword = (props) => {
     const [email, setEmail] = useState()
+    const [corEmailInvalido, setCorEmailInvalido] = useState('transparent')
+
+    const validateEmail = () => {
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+        setCorEmailInvalido('#FD7979')
+        return emailRegex.test(email)
+    }
 
     const Cadastrar = () => {
-        props.navigation.pop()
+        if (validateEmail())
+            props.navigation.pop()
     }
+
     return (
         <View style={ScreensSS.conteiner}>
             <View>
@@ -18,7 +27,7 @@ const RecoverPassword = (props) => {
                 <InputText value={email} placeholder='email@provedor.com' color='#3F92C5' width={512} height={32}
                     fontSize={12}
                     onChangeText={setEmail} keyboardType='email-address' />
-                <Label value='E-mail parece ser inválido' color='#FD7979' fontSize={14} paddingBottom={8} />
+                <Label value='E-mail parece ser inválido' color={corEmailInvalido} fontSize={14} paddingBottom={8} />
             </View>
             <Button value='RECUPERAR' color='white' iconColor='white' backgroundColor='#37BD6D' width={512} height={32}
                 size={0} fontSize={16} Execute={Cadastrar} />
