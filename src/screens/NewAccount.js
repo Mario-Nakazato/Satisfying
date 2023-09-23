8f8c7fbe723f32d3ba38b08371dcb69a290a36ce
Mario Nakazato <marionakazat@gmail.com>
2023-09-23 11:41:50 -0300

    New account screen validation

--- src/screens/NewAccount.js
+++ src/screens/NewAccount.js
@@ -6,12 +6,38 @@ import InputText from '../components/InputText'
 import Button from '../components/Button'
 
 const NewAccount = (props) => {
-    const [email, setEmail] = useState()
-    const [senha, setSenha] = useState()
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
     const [repetirSenha, setRepetirSenha] = useState()
+    const [corInvalido, setCorInvalido] = useState('transparent')
+    const [erro, setErro] = useState('')
+
+    const validateEmail = () => {
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+        setCorInvalido('#FD7979')
+        return emailRegex.test(email)
+    }
+
+    const validateInputs = () => {
+        if (!validateEmail()) {
+            setErro('E-mail parece ser inválido')
+            return false
+        }
+
+        if (senha !== repetirSenha || !senha.trim() || !repetirSenha.trim()) {
+            setErro('O campo repetir senha difere da senha')
+            setCorInvalido('#FD7979')
+            return false
+        }
+
+        setErro('')
+
+        return true
+    }
 
     const Cadastrar = () => {
-        props.navigation.pop()
+        if (validateInputs())
+            props.navigation.pop()
     }
     return (
         <View style={ScreensSS.conteiner}>
@@ -29,7 +55,7 @@ const NewAccount = (props) => {
                 <Label value='Repetir Senha' color='white' fontSize={16} paddingTop={4} />
                 <InputText value={repetirSenha} placeholder='********' color='#3F92C5' width={512} height={32} fontSize={12}
                     onChangeText={setRepetirSenha} keyboardType='visible-password' />
-                <Label value='O campo repetir senha difere da senha' color='#FD7979' fontSize={14} />
+                <Label value={erro} color={corInvalido} fontSize={14} />
             </View>
             <Button value='CADASTRAR' color='white' iconColor='white' backgroundColor='#37BD6D' width={512} height={32}
                 size={0} fontSize={16} Execute={Cadastrar} />
