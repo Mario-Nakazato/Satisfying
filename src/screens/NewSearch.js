82d53bf21e641b28a411a7ee81147fad4619525c
Mario Nakazato <marionakazat@gmail.com>
2023-11-08 10:29:49 -0300

    New search screen recording event in the database with image taken by the camera

--- src/screens/NewSearch.js
+++ src/screens/NewSearch.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import ScreensSS from '../styles/ScreensSS'
 import Label from '../components/Label'
 import InputText from '../components/InputText'
@@ -7,18 +7,19 @@ import Button from '../components/Button'
 import InputTextPaper from '../components/InputTextPaper'
 import { TextInput } from 'react-native-paper'
 import DatePicker from 'react-native-date-picker'
-
-const estilos = StyleSheet.create({
-    conteinerCadastrar: {
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-    },
-})
+import { db, store } from '../database/Config'
+import { collection, addDoc } from "firebase/firestore"
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker'
+import { uploadBytes, ref, getDownloadURL } from 'firebase/storage'
+import { useSelector } from "react-redux"
 
 const NewSearch = (props) => {
+    const { usuario } = useSelector((state) => state.usuario)
     const [nome, setNome] = useState('')
     const [seletorData, setSeletorData] = useState(new Date())
     const [data, setData] = useState('')
+    const [imagemUrl, setImagemUrl] = useState('')
+    const [imagem, setImagem] = useState('')
     const [abrirSeletor, setAbrirSeletor] = useState(false)
     const [corNomeInvalido, setCorNomeInvalido] = useState('transparent')
     const [corDataInvalida, setCorDataInvalida] = useState('transparent')
@@ -29,6 +30,14 @@ const NewSearch = (props) => {
 
     const Imagem = () => {
         console.log('Botão - Câmera/Galeria de imagens')
+        launchCamera({ mediaType: 'photo', cameraType: 'back', quality: 1 })
+            .then((result) => {
+                setImagemUrl(result.assets[0].uri)
+                setImagem(result.assets[0])
+            })
+            .catch((error) => {
+                console.log("Imagem erro: ", error)
+            })
     }
 
     const validateInputs = () => {
@@ -48,9 +57,54 @@ const NewSearch = (props) => {
         return true
     }
 
-    const Cadastrar = () => {
-        if (validateInputs())
-            props.navigation.pop()
+    const Cadastrar = async () => {
+        if (validateInputs()) {
+            if (imagem !== '') {
+                const imageRef = ref(store, imagem.fileName)
+                const file = await fetch(imagemUrl)
+                const blob = await file.blob()
+                uploadBytes(imageRef, blob, { contentType: 'image/jpeg' })
+                    .then((success) => {
+                        console.log("Upload sucesso", success)
+                        getDownloadURL(imageRef)
+                            .then((url) => {
+                                addDoc(collection(db, "events"), {
+                                    user: usuario.email,
+                                    name: nome,
+                                    date: data,
+                                    image: url
+                                })
+                                    .then((docRef) => {
+                                        console.log('Evento adicionado: ', docRef)
+                                        props.navigation.pop()
+                                    })
+                                    .catch((error) => {
+                                        console.log('Erro ao adicionar evento: ', error)
+                                    })
+                            })
+                            .catch((error) => {
+                                console.log("Download URL erro:", error)
+                            })
+                    })
+                    .catch((error) => {
+                        console.log("Upload erro: ", error)
+                    })
+            } else {
+                addDoc(collection(db, "events"), {
+                    user: usuario.email,
+                    name: nome,
+                    date: data,
+                    image: 'https://firebasestorage.googleapis.com/v0/b/satisfying-32305.appspot.com/o/semImagem.jpg?alt=media&token=3dce6265-c736-4edd-a754-0a5fb4821f8f'
+                })
+                    .then((docRef) => {
+                        console.log('Evento adicionado: ', docRef)
+                        props.navigation.pop()
+                    })
+                    .catch((error) => {
+                        console.log('Erro ao adicionar evento: ', error)
+                    })
+            }
+        }
     }
 
     return (
@@ -90,6 +144,11 @@ const NewSearch = (props) => {
                     />
                 </View>
                 <View>
+                    {/* {imagemUrl ?
+                        <Image source={{ uri: imagemUrl }} style={{ width: 100, height: 100 }} />
+                        :
+                        null
+                    } */}
                     <Label value='Imagem' color='white' fontSize={16} paddingTop={4} />
                     <Button value='Câmera/Galeria de imagens' color='#939393' iconColor='white' backgroundColor='white' width={256} height={64} size={0} fontSize={16} Execute={Imagem} />
                 </View>
@@ -99,5 +158,12 @@ const NewSearch = (props) => {
         </View>
     )
 }
+
+const estilos = StyleSheet.create({
+    conteinerCadastrar: {
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+    },
+})
 
 export default NewSearch
