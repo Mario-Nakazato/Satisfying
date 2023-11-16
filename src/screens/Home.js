6b8d3a33117631a29760ce7587eea3ee1bb65993
Mario Nakazato <marionakazat@gmail.com>
2023-11-16 16:20:09 -0300

    Home screen now searches all events related to the user in addition to the search bar working, for now simple search by exact name of the event

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,57 +1,73 @@
-import { View, StyleSheet, Text } from 'react-native'
+import { useState, useEffect } from 'react'
+import { View } from 'react-native'
 import ScreensSS from '../styles/ScreensSS'
 import EventList from '../components/EventList'
 import Button from '../components/Button'
 import InputTextPaper from '../components/InputTextPaper'
 import { TextInput } from 'react-native-paper'
-
-const linkImagem =
-	'https://boo-prod.b-cdn.net/questions/6503684e231cffaf85bde63f/16947221287341967f15b1de81112cca71f27fcb18f8e.jpg?d=400x400'
-
-const eventos = [
-	{
-		id: '001',
-		nome: 'Evento 1',
-		data: '01/01/2021',
-		imagem: linkImagem,
-	},
-	{
-		id: '002',
-		nome: 'Evento 2',
-		data: '02/01/2021',
-		imagem: linkImagem,
-	},
-	{
-		id: '003',
-		nome: 'Evento 3',
-		data: '03/01/2021',
-		imagem: linkImagem,
-	},
-	{
-		id: '004',
-		nome: 'Evento 4',
-		data: '04/01/2021',
-		imagem: linkImagem,
-	},
-	{
-		id: '005',
-		nome: 'Evento 5',
-		data: '04/01/2021',
-		imagem: linkImagem,
-	},
-]
+import { db } from '../database/Config'
+import { collection, query, onSnapshot, where } from 'firebase/firestore'
+import { useSelector } from "react-redux"
 
 const Home = (props) => {
+	const { usuario } = useSelector((state) => state.usuario)
+	const [buscar, setBuscar] = useState('')
+	const [eventos, setEventos] = useState('')
+
+	const events = collection(db, 'events')
+	useEffect(() => {
+		const q = query(events, where('user', '==', usuario.email))
+		onSnapshot(q, (snapshot) => {
+			const evento = []
+			snapshot.forEach((doc) => {
+				evento.push({
+					id: doc.id,
+					...doc.data()
+				})
+			})
+			setEventos(evento)
+		})
+	}, [])
+
 	const NovaPesquisa = () => {
 		props.navigation.push('NewSearch')
 	}
 	const Card = () => {
 		props.navigation.push('SearchActions')
 	}
+
+	const Buscar = () => {
+		if (buscar !== '') {
+			const q = query(events, where('user', '==', usuario.email), where('name', '==', buscar))
+			onSnapshot(q, (snapshot) => {
+				const evento = []
+				snapshot.forEach((doc) => {
+					evento.push({
+						id: doc.id,
+						...doc.data()
+					})
+				})
+				setEventos(evento)
+			})
+		} else {
+			const q = query(events, where('user', '==', usuario.email))
+			onSnapshot(q, (snapshot) => {
+				const evento = []
+				snapshot.forEach((doc) => {
+					evento.push({
+						id: doc.id,
+						...doc.data()
+					})
+				})
+				setEventos(evento)
+			})
+		}
+	}
+
 	return (
 		<View style={ScreensSS.conteiner}>
-			<InputTextPaper placeholder='Insira o termo de busca...'
-				textColor='black' placeholderTextColor='#8B8B8B'
+			<InputTextPaper placeholder='Insira o termo de busca...' value={buscar} onChangeText={setBuscar}
+				onSubmitEditing={Buscar} textColor='black' placeholderTextColor='#8B8B8B'
 				left={<TextInput.Icon style={{ right: 12 }} icon='magnify' color='#8B8B8B' />}
 				contentStyle={{ right: 20, fontFamily: 'AveriaLibre-Regular' }} backgroundColor='white' width={'90%'} height={24} fontSize={12} />
 			<View style={{ height: 144 }}>
