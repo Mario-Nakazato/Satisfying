0cbc54d7c8202b15162a328f49954d0abc329d06
Mario Nakazato <marionakazat@gmail.com>
2023-11-19 09:14:46 -0300

    Component event list modified to save the selected event in the global variable for use in modification, collection and reporting

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,20 +1,22 @@
 import { FlatList, StyleSheet, TouchableOpacity } from 'react-native'
 import EventItem from './EventItem'
+import { useDispatch } from 'react-redux/es/exports'
+import { reducerSetEvento } from '../redux/EventSlice'
 
 const EventList = (props) => {
-	const estilos = StyleSheet.create({
-		contentContainerStyle: {
-			alignItems: 'center',
-			paddingHorizontal: '3%'
-		},
-	})
+	const dispatch = useDispatch()
+
+	const OnPress = (item) => {
+		dispatch(reducerSetEvento({ evento: JSON.stringify(item) }))
+		props.Execute()
+	}
 
 	return (
 		<FlatList
 			contentContainerStyle={estilos.contentContainerStyle}
 			data={props.events}
 			renderItem={({ item }) => (
-				<TouchableOpacity onPress={props.Execute}>
+				<TouchableOpacity onPress={() => OnPress(item)}>
 					<EventItem nome={item.name} data={item.date} imagem={item.image} />
 				</TouchableOpacity>
 			)}
@@ -24,5 +26,12 @@ const EventList = (props) => {
 		/>
 	)
 }
+
+const estilos = StyleSheet.create({
+	contentContainerStyle: {
+		alignItems: 'center',
+		paddingHorizontal: '3%'
+	},
+})
 
 export default EventList
